d7b1b2b0191511f7dc32d70d9470418cb71497fe
Luiz Menezes <luiz.menezesf@gmail.com>
2019-09-11 11:38:28 -0300

    Add initial plugin code from template

---
+++ setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import codecs
+from setuptools import setup
+
+
+def read(fname):
+    file_path = os.path.join(os.path.dirname(__file__), fname)
+    return codecs.open(file_path, encoding='utf-8').read()
+
+
+setup(
+    name='pytest-stoq',
+    version='0.1.0',
+    author='Stoq Team',
+    author_email='stoq-devel@async.com.br',
+    maintainer='Stoq Team',
+    maintainer_email='stoq-devel@async.com.br',
+    license='GNU GPL v3.0',
+    url='https://github.com/stoq/pytest-stoq',
+    description='A plugin to pytest stoq',
+    long_description=read('README.rst'),
+    py_modules=['pytest_stoq'],
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
+    install_requires=['pytest>=3.5.0'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Pytest',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Testing',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+    ],
+    entry_points={
+        'pytest11': [
+            'stoq = pytest_stoq',
+        ],
+    },
+)
