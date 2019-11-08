08974db3220e0893b82ef52edba60e0cc4b8c221
Luiz Menezes <luiz.menezesf@gmail.com>
2019-11-08 09:50:29 -0300

    Release 0.4.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(fname):
 
 setup(
     name="pytest-stoq",
-    version="0.4.0",
+    version="0.4.1",
     author="Stoq Team",
     author_email="stoq-devel@async.com.br",
     maintainer="Stoq Team",
